9578004b420c5e1f37529eb0ea623f53e26ce08f
jianzjzhang <jianzjzhang@tencent.com>
2023-03-18 16:29:22 +0800

    fix

--- aio/ws_handler.py
+++ aio/ws_handler.py
@@ -14,11 +14,10 @@ class AioWsConsumer(WebsocketConsumer):
 
     def receive(self, text_data):
         json_data = json.loads(text_data)
-        message = json_data["message"]
         print("receive message: " + text_data)
         print("channel name: " + self.channel_name)
-        async_to_sync(msg_sender.send_msg)(self.channel_name, message["to"], message)
-        self.send(text_data = json.dumps({"message": "message sent to " + message["to"] + ": " + message}))
+        async_to_sync(msg_sender.send_msg)(self.channel_name, json_data["to"], json_data["message"])
+        self.send(text_data = json.dumps({"message": "message sent to " + json_data["to"] + ": " + json_data["message"]}))
 
     def chat_message(self, event):
         self.send(text_data = event["message"])
