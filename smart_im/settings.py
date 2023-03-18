3644705bb03fbbdba3e883bc6a6da051e54dfbfb
jianzjzhang <jianzjzhang@tencent.com>
2023-03-18 11:15:39 +0800

    add config

--- smart_im/settings.py
+++ smart_im/settings.py
@@ -78,7 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'HOST': '127.0.0.1',
-        'PORT': 3306
+        'PORT': 3306,
+        'USER': 'django',
+        'PASSWORD': 'django'
+        'NAME': 'django',
     }
 }
 
