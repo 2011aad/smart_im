76b5afd4160091870902055d93b6e1e5da0ab11a
jianzjzhang <jianzjzhang@tencent.com>
2023-03-18 11:18:19 +0800

    fix

--- smart_im/settings.py
+++ smart_im/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': 3306,
         'USER': 'django',
-        'PASSWORD': 'django'
+        'PASSWORD': 'django',
         'NAME': 'django',
     }
 }
