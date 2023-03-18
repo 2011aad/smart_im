f8b715eb9969851474d9c351dee5f07d3120dbaa
jianzjzhang <jianzjzhang@tencent.com>
2023-03-18 17:14:19 +0800

    fix

--- aio/msg_sender.py
+++ aio/msg_sender.py
@@ -1,7 +1,10 @@
 from channels.layers import get_channel_layer
+from django.core.cache import cache
+
 
 def send_msg(sender, receiver, content):
-    get_channel_layer().send(receiver, {
+    receiver_channel_name = cache.get('channel_name_' + receiver)
+    get_channel_layer().send(receiver_channel_name, {
         "type": "chat.message",
         "message": content,
     })
--- aio/ws_handler.py
+++ aio/ws_handler.py
@@ -1,6 +1,7 @@
 import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.core.cache import cache
 
 from . import msg_sender
 
@@ -14,10 +15,18 @@ class AioWsConsumer(WebsocketConsumer):
 
     def receive(self, text_data):
         json_data = json.loads(text_data)
+        if 'method' in json_data:
+            if json_data['method'] == 'set_user':
+                self.set_user(json_data['user'])
+                return
+
         print("receive message: " + text_data)
         print("channel name: " + self.channel_name)
         async_to_sync(msg_sender.send_msg)(self.channel_name, json_data["to"], json_data["message"])
         self.send(text_data = json.dumps({"message": "message sent to " + json_data["to"] + ": " + json_data["message"]}))
+
+    def set_user(self, user):
+        cache.set('channel_name_' + user, self.channel_name)
 
     def chat_message(self, event):
         self.send(text_data = event["message"])
